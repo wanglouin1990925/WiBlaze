5413e6d39e0d5602a3f0b0eab094b9491b1a85f3
Mobile and Web Developer <wanglouin1990925@hotmail.com>
2017-01-23 09:00:38 -0800

    Add files via upload

---
+++ SideMenuTableView.swift
@@ -0,0 +1,79 @@
+//
+//  SideMenuTableView.swift
+//  SideMenu
+//
+//  Created by Jon Kent on 4/5/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import SideMenu
+
+@objc protocol SideMenuTableViewDelegate {
+    
+    @objc optional func refreshClicked()
+    @objc optional func goHomeClicked()
+    @objc optional func requestDesktop()
+}
+
+class SideMenuTableView: UITableViewController {
+    
+    var delegate: SideMenuTableViewDelegate?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // this will be non-nil if a blur effect is applied
+        guard tableView.backgroundView == nil else {
+            return
+        }
+        
+        // Set up a cool background image for demo purposes
+        let imageView = UIImageView(image: UIImage(named: "Stars"))
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = UIColor.black.withAlphaComponent(0.2)
+        tableView.backgroundView = imageView
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        DispatchQueue.main.async(execute: { () -> Void in
+            self.tableView.setNeedsDisplay()
+            self.tableView.reloadData()
+        })
+    }
+    
+    @IBAction fileprivate func close() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func refresh(sender: AnyObject!) {
+        //delegate?.refreshClicked!()
+        NotificationCenter.default.post(name: Notification.Name("RefreshNotification"), object: nil)
+        close()
+        //print("Active Refresh")
+    }
+    
+    @IBAction func goHome(sender: AnyObject!) {
+        //delegate?.goHomeClicked!()
+        NotificationCenter.default.post(name: Notification.Name("GoHomeNotification"), object: nil)
+        close()
+        //print("Go to Homepage")
+    }
+    
+    @IBAction func requestDesktop(sender: AnyObject!) {
+        //delegate?.requestDesktop!()
+        NotificationCenter.default.post(name: Notification.Name("RequestDesktopNotification"), object: nil)
+        close()
+        //print("Request Desktop Site")
+    }
+    
+    @IBAction func inDevelopment(sender: AnyObject!) {
+        let alert = UIAlertController(title: "Under Development", message: "This feature is currently under development and will be available in the next update.", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Okay", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60.0
+    }
+}
---
+++ ViewController.swift
@@ -0,0 +1,284 @@
+//
+//  ViewController.swift
+//  WiBlaze
+//
+//  Created by Justin Bush on 2017-01-15.
+//  Copyright © 2017 Justin Bush. All rights reserved.
+//
+
+import UIKit
+import WebKit
+import ActivityNavigationBar
+import SideMenu
+
+class ViewController: UIViewController, UINavigationControllerDelegate, WKNavigationDelegate, UITextFieldDelegate, SideMenuTableViewDelegate {
+
+    let defaults = UserDefaults.standard
+    
+    var webView: WKWebView!
+
+    @IBOutlet var addressBar: UITextField!
+    @IBOutlet var backButton: UIBarButtonItem!
+    
+    // App Screen Bounds
+    var appScreenRect: CGRect {
+        let appWindowRect = UIApplication.shared.keyWindow?.bounds ?? UIWindow().bounds
+        return appWindowRect
+    }
+    
+    override func loadView() {
+        // Setup WebKit
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.navigationDelegate = self
+        webConfiguration.preferences.javaScriptEnabled = true
+        self.view = webView
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name("RefreshNotification"), object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name("GoHomeNotification"), object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name("RequestDesktopNotification"), object: nil)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    
+        NotificationCenter.default.addObserver(self, selector: #selector(self.receiveRefreshNotification(notification:)), name: NSNotification.Name("RefreshNotification"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.receiveGoHomeNotification(notification:)), name: NSNotification.Name("GoHomeNotification"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.receiveRequestDesktopNotification(notification:)), name: NSNotification.Name("RequestDesktopNotification"), object: nil)
+    }
+    
+    func receiveRefreshNotification(notification: Notification){
+        
+        print("Refresh Page")
+    }
+    
+    func receiveGoHomeNotification(notification: Notification){
+        
+        print("Refresh Page")
+    }
+    
+    func receiveRequestDesktopNotification(notification: Notification){
+        
+        print("Refresh Page")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addressBar.delegate = self
+        var homepageURL: String!
+        
+        // Set Placeholder Style
+        addressBar.attributedPlaceholder = NSAttributedString(string:" Search or type URL", attributes: [NSForegroundColorAttributeName: UIColor.gray])
+        
+        // Set Homepage
+        if defaults.bool(forKey: "customHomepage") {
+            
+            if let homepageURL = defaults.string(forKey: "homepageURL") {
+                // Custom Homepage is Enabled
+                let webURL = URL(string: homepageURL)
+                let webRequest = URLRequest(url: webURL!)
+                webView.load(webRequest)
+                print("Load Custom Homepage:", homepageURL)
+                
+            } else {
+                // No Custom Homepage, Load Default
+                homepageURL = "https://google.com"
+                let webURL = URL(string: homepageURL)
+                let webRequest = URLRequest(url: webURL!)
+                webView.load(webRequest)
+            }
+            
+        } else {
+            // No Custom Homepage, Load Default
+            homepageURL = "https://google.com"
+            let webURL = URL(string: homepageURL)
+            let webRequest = URLRequest(url: webURL!)
+            webView.load(webRequest)
+        }
+        
+        // Set WebKit Load Configurations
+        webView.allowsBackForwardNavigationGestures = true
+        
+        // Setup Menu Controller
+        let menuRightNavigationController = UISideMenuNavigationController()
+        SideMenuManager.menuRightNavigationController = menuRightNavigationController
+        SideMenuManager.menuBlurEffectStyle = .dark
+        SideMenuManager.menuPresentMode = .menuSlideIn
+        
+        // Set Menu Width (eg. * 0.65 = 65%)
+        SideMenuManager.menuWidth = max(round(min((appScreenRect.width), (appScreenRect.height)) * 0.65), 240)
+    }
+    
+    // Setup Address Bar
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        addressBar.resignFirstResponder()
+        print("User Requested:", addressBar.text!)
+        loadURL(address: addressBar.text!)
+        return true
+    }
+
+    // Toggle Back Button
+    func checkBack() {
+        if webView.canGoBack {
+            backButton.isEnabled = true
+        } else {
+            backButton.isEnabled = false
+        }
+    }
+    
+    // Validate URL
+    func validateURL(urlString: String) -> Bool {
+        let urlRegEx = "((?:http|https)://)?(?:www\\.)?[\\w\\d\\-_]+\\.\\w{2,3}(\\.\\w{2})?(/(?<=/)(?:[\\w\\d\\-./_]+)?)?"
+        return NSPredicate(format: "SELF MATCHES %@", urlRegEx).evaluate(with: urlString)
+    }
+    
+    // Format URL Scheme
+    func formatURL(userRequest: String) -> String {
+        
+        var formattedURL: String
+        
+        // Check if userRequest only contains URL prefixes
+        let prefixes = ["http://", "https://", "www.", "http://www.", "https://www."]
+        
+        if validateURL(urlString: userRequest) {
+            // userRequest is a valid URL
+            if prefixes.contains(userRequest) {
+                // URL does have prefix
+                formattedURL = userRequest
+            } else {
+                // Format URL and add prefix
+                formattedURL = "http://" + userRequest
+            }
+            
+            print("Load URL:", formattedURL)
+            return formattedURL
+            
+        } else {
+            
+            // userRequest is not a valid URL, load search query
+            let searchQuery = userRequest.replacingOccurrences(of: " ", with: "+", options: .literal, range: nil)
+            let searchEngine = "https://google.com/#q="
+            let searchURL = searchEngine + searchQuery
+            print("Load Search:", searchURL)
+            return searchURL
+        }
+    }
+    
+    // Load URL in the WebView
+    func loadURL(address: String) {
+        let address = formatURL(userRequest: address)
+        let webURL = URL(string: address)
+        let webRequest = URLRequest(url: webURL!)
+        webView.load(webRequest)
+    }
+    
+    // WebView Called for Navigation
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        activityNavigationBar?.startActivity(andWaitAt: 0.8)
+        
+        //if webView.url?.absoluteString.hasPrefix("https://")
+        activityNavigationBar?.activityBarColor = UIColor.white
+    }
+    
+    // WebView Started Downloading Content
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        checkBack()
+        
+        if webView.estimatedProgress < 0.8 {
+            activityNavigationBar?.finishActivity(withDuration: 0.8)
+        } else {
+            activityNavigationBar?.finishActivity(withDuration: webView.estimatedProgress)
+        }
+    }
+    
+    // WebView Finished Loading
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        addressBar.text = webView.url?.absoluteString
+        activityNavigationBar?.reset()
+    }
+    
+    // Setup Activity Navigation
+    var activityNavigationBar: ActivityNavigationBar? {
+        return navigationController?.navigationBar as? ActivityNavigationBar
+    }
+    
+    // Set Status Bar Style
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
+    
+    // Go Back to Previous WebPage
+    @IBAction func goBack(sender: AnyObject) {
+        webView.goBack()
+    }
+    
+    // Refresh WebView Content
+    func refreshClicked() {
+        webView.reload()
+        print("Refresh Page")
+    }
+    
+    func goHomeClicked() {
+        var homepageURL: String!
+        
+        // Set Homepage
+        if defaults.bool(forKey: "customHomepage") {
+            
+            if let homepageURL = defaults.string(forKey: "homepageURL") {
+                // Custom Homepage is Enabled
+                let webURL = URL(string: homepageURL)
+                let webRequest = URLRequest(url: webURL!)
+                webView.load(webRequest)
+                print("Load Custom Homepage:", homepageURL)
+                
+            } else {
+                // No Custom Homepage, Load Default
+                homepageURL = "https://google.com"
+                let webURL = URL(string: homepageURL)
+                let webRequest = URLRequest(url: webURL!)
+                webView.load(webRequest)
+            }
+            
+        } else {
+            // No Custom Homepage, Load Default
+            homepageURL = "https://google.com"
+            let webURL = URL(string: homepageURL)
+            let webRequest = URLRequest(url: webURL!)
+            webView.load(webRequest)
+        }
+    }
+    
+    func requestDesktop() {
+        let desktopAgent: String! = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/602.3.12 (KHTML, like Gecko) Version/10.0.2 Safari/602.3.12"
+        
+        // Set Desktop UserAgent
+        defaults.register(defaults: ["UserAgent": desktopAgent])
+        
+        // Restart UIWebView
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+
+        if segue.destination is UISideMenuNavigationController {
+            //vc.delegate = self
+            
+            print("111111111")
+            
+        }
+    }
+}
